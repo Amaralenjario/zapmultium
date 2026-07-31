538a007147de33a38dc7b2a3f7fb4f8f0bb071fc
amaralenda <amaral.ricardo.ig@gmail.com>
2026-07-31 15:57:24 -0300

    fix: instanceMap NC-CAIO com phone_number_id correto (nao string vazia)

--- src/lib/instances.ts
+++ src/lib/instances.ts
@@ -3,7 +3,7 @@ const instanceMap: Record<string, { name: string; channelId: string; channelToke
   "892228177298374": { name: "GUSTAVO - LUIS", channelId: "c5505ddf-f9ef-4837-9337-45ed3de40d6a", channelToken: "ff852b8e48eea68b9c0d1e2f3a4b5c6d7" },
   "1034222499765101": { name: "AMANDA - JÉ", channelId: "346e4eef-bc78-41ec-a7ae-ec7ec75bf177", channelToken: "a4a499ac2e7791df0e1f2a3b4c5d6e7f" },
   "976034132269824": { name: "GABI - 8176", channelId: "effa72d1-47f6-445b-acbc-7693ef21ee24", channelToken: "573ce80fda8e16ab0d1e2f3a4b5c6d7e8" },
-  "": { name: "NC - CAIO", channelId: "b1c6879b-e962-4f50-95f7-14f1a04601a5", channelToken: "031d4b9bed27fdce0f1e2a3b4c5d6e7f8" },
+  "1234821229708132": { name: "NC - CAIO", channelId: "b1c6879b-e962-4f50-95f7-14f1a04601a5", channelToken: "031d4b9bed27fdce0f1e2a3b4c5d6e7f8" },
 };
 
 export function getInstanceByPhoneId(phoneNumberId: string) {
